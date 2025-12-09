d7d8fb976645d00c01f0dbc81997f52b5335a0da
Cameron Peek <cameron.peek15@gmail.com>
2025-12-09 03:56:29 -0500

    rebased onto main branch, starting java refactor

---
+++ src/com/library/checkout/user/User.java
@@ -0,0 +1,7 @@
+package com.library.checkout.user;
+
+// User.java
+// Cameron Peek
+// 12/9/2025
+public record User (int id, String name, String password, String roles) {
+}
---
+++ src/com/library/checkout/user/UserService.java
@@ -0,0 +1,7 @@
+package com.library.checkout.user;
+
+// UserService.java
+// Cameron Peek
+// 12/9/2025
+public class UserService {
+}
