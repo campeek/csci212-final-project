29592e51c3f7070f0787292b8652ffd9bb8e5e4b
Qcifer <trhisten.baumgartner@gmail.com>
2025-12-10 21:22:04 -0500

    Update Book.java
    added file handling

--- src/com/library/checkout/book/Book.java
+++ src/com/library/checkout/book/Book.java
@@ -1,48 +1,134 @@
 package com.library.checkout.book;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.PrintWriter;
+import java.nio.file.StandardOpenOption;
 
 public class Book {
 	private String author;
 	private String title;
 	private int serial_number;
+	private boolean checked_out;
 	public Book(){
 		this.author="";
 		this.title="";
 		this.serial_number=-1;
+		this.checked_out=false;
 	}
-	public Book(String author_in, String title_in, int serial_number_in){
+	public Book(String author_in, String title_in, int serial_number_in, boolean check){
 		this.author=author_in;
 		this.title=title_in;
 		this.serial_number=serial_number_in;
+		this.checked_out=check;
 	}
 	public Book(String whole_line) {
 		try {
-		String[] temp=whole_line.split(",");
-		this.author=temp[0];
-		this.title=temp[1];
-		this.serial_number=Integer.parseInt(temp[2]);
+			String[] temp=whole_line.split(",");
+			this.author=temp[0];
+			this.title=temp[1];
+			this.serial_number=Integer.parseInt(temp[2]);
+			if(temp[3]=="true"){
+				this.checked_out=true;
+			}
+			else{
+				this.checked_out=false;
+			}
 		}
 		catch(Exception e) {
-			System.err.println("error converting whole string to book. remember to use the format 'author,title,number'");
+			System.err.println("error converting whole string to Book. remember to use the format 'author,title,number'");
 		}
 	}
 	public String get_author(){
-		return author;
+		return this.author;
 	}
 	public void set_author(String author_in){
 		this.author=author_in;
 	}
 	public String get_title(){
-		return title;
+		return this.title;
 	}
 	public void set_title(String title_in){
 		this.title=title_in;
 	}
 	public int get_serial_number(){
-		return serial_number;
+		return this.serial_number;
 	}
 	public void set_serial_number(int serial_number_in){
 		this.serial_number=serial_number_in;
 	}
-	//TODO: make the bits that write to and read from files.
+	public boolean get_checked_out(){
+		return this.checked_out;
+	}
+	public void set_checked_out(boolean check){
+		this.checked_out=check;
+	}
+	public String to_string(){
+		return(this.author +","+ this.title +","+this.serial_number +","+this.checked_out);
+	}
+	public Vector read_books_from_file(){
+		Vector<Book> Book_list = new Vector<>();
+		String temp_line;
+		String temp_auth;
+		String temp_title;
+		int temp_SN;
+		boolean temp_check;
+		try (Scanner scanner = new Scanner(new File("FIXME INSERT FILEPATH HERE"))) {
+        	while (scanner.hasNextLine()) {
+                temp_line = scanner.nextLine();
+				try {
+					String[] temp=temp_line.split(",");
+					temp_auth=temp[0];
+					temp_title=temp[1];
+					temp_SN=Integer.parseInt(temp[2]);
+					if(temp[3]=="true"){
+						temp_check=true;
+					}
+					else{
+						temp_check=false;
+					}
+					Book temp_book = new Book(temp_auth,temp_title,temp_SN,temp_check);
+				    Book_list.add(temp_book);
+				}
+				catch(Exception e) {
+					System.err.println("error converting whole string to Book. remember to use the format 'author,title,number,boolean'");
+					break;
+				}	
+            }
+        } 
+		catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
+		return Book_list;
+	}
+	public void write_books_to_file(Vector<Book> list){ //clears book.txt, fills it in with books from the vector that is fed into it
+		try {
+	        FileWriter file_writer = new FileWriter("Books.txt", false);
+			PrintWriter printWriter = new PrintWriter(file_writer);
+			for (Book element : list) {
+        		printWriter.println(element.to_string()); 
+        	}
+    	}
+		catch (IOException e) {
+        	System.err.println("An IOException occurred: " + e.getMessage());
+    	}
+	}
+	public void write_book_to_file(Book book){ //appends one book to the end of the book file.
+		try {
+	        FileWriter file_writer = new FileWriter("Books.txt", true);
+        	PrintWriter printWriter = new PrintWriter(file_writer);
+        	printWriter.println(book.to_string()); // Writes the element followed by a new line			
+    	}
+		catch (IOException e) {
+        	System.err.println("An IOException occurred: " + e.getMessage());
+    	}
+	}
 }
-
